13f8361e53ec20369c27217cd8ba3f86fce0bd9b
tfboyd <tobyboyd@google.com>
2017-01-22 09:41:03 -0800

    Added back some stuff needed from mxnet

--- benchmark/runscalabilitytest.sh
+++ benchmark/runscalabilitytest.sh
@@ -115,14 +115,14 @@ fi
 echo "Using $REMOTE_DIR as remote directory"
 
 
-#if [ ! -d "mxnet" ]; then
-#    echo "Cloning MXNet"
-#    git clone https://github.com/dmlc/mxnet.git
-#    cd mxnet && git reset --hard a3a928c21ab91b246a5fab7c9ec135f6e616f899
-#    git clone https://github.com/dmlc/dmlc-core dmlc-core
-#    cd dmlc-core && git reset --hard f554de0a6914f8028aab50aea02003a4344e732d
-#    cd ../..
-#fi
+if [ ! -d "mxnet" ]; then
+    echo "Cloning MXNet"
+    git clone https://github.com/dmlc/mxnet.git
+    cd mxnet && git reset --hard a3a928c21ab91b246a5fab7c9ec135f6e616f899
+    git clone https://github.com/dmlc/dmlc-core dmlc-core
+    cd dmlc-core && git reset --hard f554de0a6914f8028aab50aea02003a4344e732d
+    cd ../..
+fi
 
 # Create the hostname list required for MXNet
 rm -f hostnames
@@ -135,33 +135,34 @@ while read line; do
 done
 
 
-#echo "Compressing MXNet"
-#rm -f mxnet.tar.gz
-#tar -cvzf mxnet.tar.gz ./mxnet > /dev/null 2>&1
+echo "Compressing MXNet"
+rm -f mxnet.tar.gz
+tar -cvzf mxnet.tar.gz ./mxnet > /dev/null 2>&1
 
-#echo "Copying MXNet to remote nodes..."
-#head -$HOSTS_COUNT $HOSTS |
-#while read line; do
-#    if [ -z line ]; then continue; fi
-#    arr=( $line )
-#    ssh_alias=${arr[1]}
-#
-#    scp -o "StrictHostKeyChecking no" mxnet.tar.gz $ssh_alias:$REMOTE_DIR
-#    scp -o "StrictHostKeyChecking no" hostnames $ssh_alias:$REMOTE_DIR
-#    ssh -o "StrictHostKeyChecking no" $ssh_alias 'cd '${REMOTE_DIR}' && tar -xvzf mxnet.tar.gz > /dev/null 2>&1' &
-#done
+echo "Copying MXNet to remote nodes..."
+head -$HOSTS_COUNT $HOSTS |
+while read line; do
+    if [ -z line ]; then continue; fi
+    arr=( $line )
+    ssh_alias=${arr[1]}
+
+    scp -o "StrictHostKeyChecking no" mxnet.tar.gz $ssh_alias:$REMOTE_DIR
+    scp -o "StrictHostKeyChecking no" hostnames $ssh_alias:$REMOTE_DIR
+    ssh -o "StrictHostKeyChecking no" $ssh_alias 'cd '${REMOTE_DIR}' && tar -xvzf mxnet.tar.gz > /dev/null 2>&1' &
+done
 
 # Construct the models string for MXNet 
-#mxnet_model_string=$MODELS
-#mxnet_model_string=`echo $mxnet_model_string | sed "s/Alexnet/alexnet/g"`
-#mxnet_model_string=`echo $mxnet_model_string | sed "s/Inceptionv3/inception-v3/g"`
-#mxnet_model_string=`echo $mxnet_model_string | sed "s/Resnet/resnet/g"`
-#mxnet_model_string=`echo $mxnet_model_string | sed "s/inception-v3:[0-9]*/&:299/g"`
-#mxnet_model_string=`echo $mxnet_model_string | sed "s/alexnet:[0-9]*/&:224/g"`
-#mxnet_model_string=`echo $mxnet_model_string | sed "s/resnet:[0-9]*/&:224/g"`
-#mxnet_model_string=`echo $mxnet_model_string | sed "s/,/' '/g"`
-#mxnet_model_string="'"${mxnet_model_string}"'"
+mxnet_model_string=$MODELS
+mxnet_model_string=`echo $mxnet_model_string | sed "s/Alexnet/alexnet/g"`
+mxnet_model_string=`echo $mxnet_model_string | sed "s/Inceptionv3/inception-v3/g"`
+mxnet_model_string=`echo $mxnet_model_string | sed "s/Resnet/resnet/g"`
+mxnet_model_string=`echo $mxnet_model_string | sed "s/inception-v3:[0-9]*/&:299/g"`
+mxnet_model_string=`echo $mxnet_model_string | sed "s/alexnet:[0-9]*/&:224/g"`
+mxnet_model_string=`echo $mxnet_model_string | sed "s/resnet:[0-9]*/&:224/g"`
+mxnet_model_string=`echo $mxnet_model_string | sed "s/,/' '/g"`
+mxnet_model_string="'"${mxnet_model_string}"'"
 
+echo "skipping actuall running of MXNet"
 # Construct the command to run MXNet tests
 #image_recog_dir="${REMOTE_DIR}/mxnet/example/image-classification/"
 #mxnet_command="cd $image_recog_dir && python benchmark.py --worker_file ${REMOTE_DIR}/hostnames --worker_count ${HOSTS_COUNT} --gpu_count ${GPU_PER_HOST} --networks ${mxnet_model_string}"
